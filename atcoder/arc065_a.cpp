f7ed6c1837ca0266daff8dfffc48b605a832296e
manabuide <89676816+manabuide@users.noreply.github.com>
2021-09-06 01:12:33 +0900

    Make answer_1 function

--- atcoder/arc065_a.cpp
+++ atcoder/arc065_a.cpp
@@ -1,31 +1,35 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+void answer_1(string S) {
+    int len = S.size();
+
+    while(true) {
+        if(S.substr(len - 5, 5) == "dream") {
+            len -= 5;
+        } else if(S.substr(len - 5, 5) == "erase") {
+            len -= 5;
+        } else if(S.substr(len - 6, 6) == "eraser") {
+            len -= 6;
+        } else if(S.substr(len - 7, 7) == "dreamer") {
+            len -= 7;
+        } else {
+            cout << "NO" << endl;
+            break;
+        }
+
+        if(len <= 0) {
+            cout << "YES" << endl;
+            break;
+        }
+    }
+}
+
 int main() {
     string S;
-	cin >> S;
+    cin >> S;
 
-	int len = S.size();
+	answer_1(S);
 
-	while (true) {
-		if (S.substr(len - 5, 5) == "dream") {
-			len -= 5;
-		} else if (S.substr(len - 5, 5) == "erase") {
-			len -= 5;
-		} else if (S.substr(len - 6, 6) == "eraser") {
-			len -= 6;
-		} else if (S.substr(len - 7, 7) == "dreamer") {
-			len -= 7;
-		} else {
-			cout << "NO" << endl;
-			break;
-		}
-
-		if (len <= 0) {
-			cout << "YES" << endl;
-			break;
-		}
-	}	
-	
-	return 0;
+    return 0;
 }
