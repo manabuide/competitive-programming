53e4a0b0ee66e6df2cf187b3d0e17668d88d7c7c
manabuide <89676816+manabuide@users.noreply.github.com>
2021-09-08 00:16:03 +0900

    Add b.cpp

---
+++ atcoder/abc/007/b.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	string A;
+	cin >> A;
+
+	if (A[0] == 'a' && A.size() == 1) {
+		cout << -1 << endl;
+	} else {
+		cout << 'a' << endl;
+	}
+
+	return 0;
+}
