00a154a2c7b74a1aca54a679acf15c14234fefc7
manabuide <89676816+manabuide@users.noreply.github.com>
2021-09-17 10:38:15 +0900

    Add b.cpp
    Pending. WA * 1.

---
+++ atcoder/abc/068/b.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int number;
+	cin >> number;
+
+	int max_count = 0;
+	int answer = 0;
+
+	for (int x = number; x > 0; x--) {
+		int count = 0;
+		int local_x = x;  
+
+		while (true) {
+			if (local_x % 2 == 0) {
+				local_x /= 2;
+				count++;
+			} else {
+				break;
+			}
+		}
+
+		if (max_count < count) {
+			max_count = count;
+			answer = x;
+		}
+	} 
+
+	cout << answer << endl;
+
+    return 0;
+}
