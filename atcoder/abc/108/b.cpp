aa9a2bf04c8aa137a7e37caa7fcc00e871e1ee53
manabuide <89676816+manabuide@users.noreply.github.com>
2021-09-20 19:45:01 +0900

    Add b.cpp
    No answer.

---
+++ atcoder/abc/108/b.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a = 0, b = 0, c = 0, d = 1;
+    int x = c - a, y = d - b;
+
+	cout << c - y << " " << d + x << " " << a - y << " " << b + x << endl;
+
+    return 0;
+}
