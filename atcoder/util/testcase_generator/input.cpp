d6cf83dfd120c353002d4f4e28e64574113d0df3
manabuide <89676816+manabuide@users.noreply.github.com>
2021-09-23 18:00:48 +0900

    Add new line or not argument to generate_random_digit_string function

--- atcoder/util/testcase_generator/input.cpp
+++ atcoder/util/testcase_generator/input.cpp
@@ -9,13 +9,9 @@ long long set_random_integer_value(long long min, long long max) {
     return rnd(mt);
 }
 
-void generater_max_integer_value(long long max) {
-    cout << max << endl;
-}
+void generater_max_integer_value(long long max) { cout << max << endl; }
 
-void generater_min_integer_value(long long min) {
-    cout << min << endl;
-}
+void generater_min_integer_value(long long min) { cout << min << endl; }
 
 void generate_random_integer_value(long long min, long long max) {
     cout << set_random_integer_value(min, max) << endl;
@@ -92,32 +88,35 @@ void generate_random_integer_array(long long n, long long min, long long max,
     }
 }
 
-void generate_random_digit_string(int n) {
-    for (int i = 0; i < n; i++) {
+void generate_random_digit_string(int n, bool new_line) {
+    for(int i = 0; i < n; i++) {
         cout << char(set_random_integer_value(0, 9) + '0');
     }
-    cout << endl;
+
+    if(new_line) {
+        cout << endl;
+    }
 }
 
 void generate_random_lower_string(int n) {
-    for (int i = 0; i < n; i++) {
+    for(int i = 0; i < n; i++) {
         cout << char(set_random_integer_value(0, 25) + 'a');
     }
     cout << endl;
 }
 
 void generate_random_upper_string(int n) {
-    for (int i = 0; i < n; i++) {
+    for(int i = 0; i < n; i++) {
         cout << char(set_random_integer_value(0, 25) + 'A');
     }
     cout << endl;
 }
 
 void generate_random_string(int n) {
-    for (int i = 0; i < n; i++) {
+    for(int i = 0; i < n; i++) {
         int x = set_random_integer_value(0, 20);
 
-        if (x < 10) {
+        if(x < 10) {
             cout << char(set_random_integer_value(0, 25) + 'A');
         } else {
             cout << char(set_random_integer_value(0, 25) + 'a');
@@ -129,17 +128,15 @@ void generate_random_string(int n) {
 int main() {
     int n;
     cin >> n;
-    
+
     for(int i = 0; i < n; i++) {
         string file_name;
         file_name += "input_" + to_string(i) + ".txt";
         ofstream ofstr(file_name.c_str());
         streambuf *strbuf = cout.rdbuf(ofstr.rdbuf());
 
-
-
         cout.rdbuf(strbuf);
     }
-    
+
     return 0;
 }
