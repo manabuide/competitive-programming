962f481fdae47d0eebc2565705892d6aa23bcf13
manabuide <89676816+manabuide@users.noreply.github.com>
2021-10-07 00:51:41 +0900

    Add b.cpp

---
+++ atcoder/abc/214/b/b.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int s, t;
+	cin >> s >> t;
+
+    int count = 0;
+
+    for(int a = 0; a <= 100; a++) {
+        for(int b = 0; b <= 100; b++) {
+            for(int c = 0; c <= 100; c++) {
+                if(a + b + c <= s && a * b * c <= t) {
+                    count++;
+                }
+            }
+        }
+    }
+
+    cout << count << endl;
+
+    return 0;
+}
