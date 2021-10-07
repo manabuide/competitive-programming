159b4239aed8406fd44e4a3371671fb511d691dd
manabuide <89676816+manabuide@users.noreply.github.com>
2021-10-08 02:32:53 +0900

    Add b.cpp

---
+++ atcoder/abc/216/b/b.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    string s[n], t[n];
+    for(int i = 0; i < n; i++) {
+        cin >> s[i] >> t[i];
+    }
+
+    for(int i = 0; i < n - 1; i++) {
+        for(int j = i + 1; j < n; j++) {
+            if(s[i] == s[j] && t[i] == t[j]) {
+                cout << "Yes" << endl;
+                return 0;
+            }
+        }
+    }
+
+    cout << "No" << endl;
+
+    return 0;
+}
