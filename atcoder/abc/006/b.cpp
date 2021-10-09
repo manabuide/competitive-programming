4ada84c352113a2b8810f9248359cb6960068eae
manabuide <89676816+manabuide@users.noreply.github.com>
2021-10-10 02:27:15 +0900

    Move main function to solve1 function

--- atcoder/abc/006/b.cpp
+++ atcoder/abc/006/b.cpp
@@ -1,25 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-	long long a[3] = {0, 0, 1};
-	int n = 100000;
+void solve1() {
+    long long a[3] = {0, 0, 1};
+    int n = 100000;
 
-	if (n < 4) {
-		cout << a[n - 1] << endl;
+    if (n < 4) {
+        cout << a[n - 1] << endl;
 
-		return 0;
-	}
+        return 0;
+    }
 
-    for(int i = 3; i < n; i++) {
-		int tmp = a[0] + a[1] + a[2];
-		tmp %= 10007;
-		a[0] = a[1];
-		a[1] = a[2];
-		a[2] = tmp;
-	}
+    for (int i = 3; i < n; i++) {
+        int tmp = a[0] + a[1] + a[2];
+        tmp %= 10007;
+        a[0] = a[1];
+        a[1] = a[2];
+        a[2] = tmp;
+    }
 
-	cout << a[2] << endl;
-
-    return 0;
+    cout << a[2] << endl;
 }
+
+int main() { return 0; }
