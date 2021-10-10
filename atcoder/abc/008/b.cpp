b6ffcf0e04898ec22ac22ca516ddb52084eb1d3d
manabuide <89676816+manabuide@users.noreply.github.com>
2021-10-10 18:13:07 +0900

    Add solve2 function

--- atcoder/abc/008/b.cpp
+++ atcoder/abc/008/b.cpp
@@ -21,4 +21,27 @@ void solve1() {
     cout << answer << endl;
 }
 
+void solve2() {
+    int n;
+    std::cin >> n;
+    std::multiset<std::string> ms;
+
+    for (int i = 0; i < n; ++i) {
+        std::string s;
+        std::cin >> s;
+        ms.insert(s);
+    }
+
+    std::string name;
+    unsigned int max_count = 0;
+    for (auto iter = std::begin(ms); iter != std::end(ms); ++iter) {
+        if (max_count < ms.count(*iter)) {
+            max_count = ms.count(*iter);
+            name = *iter;
+        }
+    }
+
+    std::cout << name << "\n"s;
+}
+
 int main() { return 0; }
