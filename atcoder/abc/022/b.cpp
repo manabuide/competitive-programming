3e8c68cad32d576aec367052ce244432048bf239
manabuide <89676816+manabuide@users.noreply.github.com>
2021-10-11 01:59:47 +0900

    Move main function to solve1 function

--- atcoder/abc/022/b.cpp
+++ atcoder/abc/022/b.cpp
@@ -1,24 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-	int N;
-	cin >> N;
+void solve1() {
+    int N;
+    cin >> N;
 
-	set<int> st;
-	int count = 0;
+    set<int> st;
+    int count = 0;
 
-	for (int i = 0; i < N; i++) {
-		int A;
-		cin >> A;
+    for (int i = 0; i < N; i++) {
+        int A;
+        cin >> A;
 
-		if (st.count(A)) {
-			count++;
-		}
-		st.insert(A);
-	}
+        if (st.count(A)) {
+            count++;
+        }
+        st.insert(A);
+    }
 
-	cout << count << endl;
-
-	return 0;
+    cout << count << endl;
 }
+int main() { return 0; }
