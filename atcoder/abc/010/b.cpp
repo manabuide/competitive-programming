e69b865e37d2189ecbd86c2c98079bbd2ae6bf6b
manabuide <89676816+manabuide@users.noreply.github.com>
2021-10-11 23:06:24 +0900

    Add solve2 function

--- atcoder/abc/010/b.cpp
+++ atcoder/abc/010/b.cpp
@@ -19,4 +19,49 @@ void solve1() {
     cout << count << endl;
 }
 
+void solve2() {
+    int n;
+    cin >> n;
+
+    vector<int> a;
+
+    for (int i = 0; i < n; ++i) {
+        int temp;
+        cin >> temp;
+        a.push_back(temp);
+    }
+
+    auto pattern1 = [](const int x) {
+        if (x % 2 == 0) {
+            return 1;
+        } else {
+            return 0;
+        }
+    };
+
+    auto pattern2 = [](const int x) {
+        if (x % 3 == 2) {
+            return 1;
+        } else {
+            return 0;
+        }
+    };
+
+    int count = 0;
+
+    for (int i = 0; i < n; ++i) {
+        for (int x = a[i]; x > 0; --x) {
+            if (pattern1(x)) {
+                ++count;
+            } else if (pattern2(x)) {
+                ++count;
+            } else {
+                break;
+            }
+        }
+    }
+
+    cout << count << "\n"s;
+}
+
 int main() { return 0; }
