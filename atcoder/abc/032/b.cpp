073db6ff95ca18ced3cd5f00e3733293b9e8120c
manabuide <89676816+manabuide@users.noreply.github.com>
2021-10-13 01:17:22 +0900

    Move main function to solve1 function

--- atcoder/abc/032/b.cpp
+++ atcoder/abc/032/b.cpp
@@ -1,25 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-	string s;
-	int k;
-	cin >> s >> k;
+void solve1() {
+    string s;
+    int k;
+    cin >> s >> k;
 
-	if (k > s.size()) {
-		cout << 0 << endl;
+    if (k > s.size()) {
+        cout << 0 << endl;
 
-		return 0;
-	}
+        return 0;
+    }
 
-	set<string> st;
-	for (int i = 0; i < s.size() - k + 1; i++) {
-		string temp = s.substr(i, k);
+    set<string> st;
+    for (int i = 0; i < s.size() - k + 1; i++) {
+        string temp = s.substr(i, k);
 
-		st.insert(temp);
-	}
+        st.insert(temp);
+    }
 
-	cout << st.size() << endl;
-
-	return 0;
+    cout << st.size() << endl;
 }
+
+int main() { return 0; }
