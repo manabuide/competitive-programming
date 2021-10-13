3d28bf3c714a149449e9da5aecfb9c0d98b76f72
manabuide <89676816+manabuide@users.noreply.github.com>
2021-10-14 02:13:31 +0900

    Move main function to solve1 function

--- atcoder/abc/068/b.cpp
+++ atcoder/abc/068/b.cpp
@@ -1,33 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
+void solve1() {
     int number;
-	cin >> number;
+    cin >> number;
 
-	int max_count = 0;
-	int answer = 0;
+    int max_count = 0;
+    int answer = 0;
 
-	for (int x = number; x > 0; x--) {
-		int count = 0;
-		int local_x = x;  
+    for (int x = number; x > 0; x--) {
+        int count = 0;
+        int local_x = x;
 
-		while (true) {
-			if (local_x % 2 == 0) {
-				local_x /= 2;
-				count++;
-			} else {
-				break;
-			}
-		}
+        while (true) {
+            if (local_x % 2 == 0) {
+                local_x /= 2;
+                count++;
+            } else {
+                break;
+            }
+        }
 
-		if (max_count < count) {
-			max_count = count;
-			answer = x;
-		}
-	} 
+        if (max_count < count) {
+            max_count = count;
+            answer = x;
+        }
+    }
 
-	cout << answer << endl;
-
-    return 0;
+    cout << answer << endl;
 }
+
+int main() { return 0; }
