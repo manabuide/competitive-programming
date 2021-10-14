e3ea398ae88ec5b31f46fa9fde7c67b9699c8422
manabuide <89676816+manabuide@users.noreply.github.com>
2021-10-15 01:19:46 +0900

    Move main function to solve1 funciton

--- atcoder/abc/071/b.cpp
+++ atcoder/abc/071/b.cpp
@@ -1,35 +1,35 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-	string alphabet = "abcdefghijklmnopqrstuvwxyz"; 
-	
-	string s;
-	cin >> s;
+void solve1() {
+    string alphabet = "abcdefghijklmnopqrstuvwxyz";
 
-	set<char> st;
-	for (int str_index = 0; str_index < s.size(); str_index++) {
-		st.insert(s[str_index]);
-	}
+    string s;
+    cin >> s;
 
-	vector<char> v;
-	for (auto c : st) {
-		v.push_back(c);
-	}
+    set<char> st;
+    for (int str_index = 0; str_index < s.size(); str_index++) {
+        st.insert(s[str_index]);
+    }
 
-	char answer = ' ';
-	for (int v_index = 0; v_index < v.size(); v_index++) {
-		if (v[v_index] != alphabet[v_index]) {
-			answer = alphabet[v_index];
-			break;
-		}
-	}
+    vector<char> v;
+    for (auto c : st) {
+        v.push_back(c);
+    }
 
-	if (answer != ' ') {
-		cout << answer << endl;
-	} else {
-		cout << "None" << endl;
-	}
+    char answer = ' ';
+    for (int v_index = 0; v_index < v.size(); v_index++) {
+        if (v[v_index] != alphabet[v_index]) {
+            answer = alphabet[v_index];
+            break;
+        }
+    }
 
-    return 0;
+    if (answer != ' ') {
+        cout << answer << endl;
+    } else {
+        cout << "None" << endl;
+    }
 }
+
+int main() { return 0; }
