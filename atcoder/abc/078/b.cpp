486fd9ec0bc373c5c255334914e5a2f2e30f60ec
manabuide <89676816+manabuide@users.noreply.github.com>
2021-10-15 23:23:32 +0900

    Move main function to solve1 function

--- atcoder/abc/078/b.cpp
+++ atcoder/abc/078/b.cpp
@@ -1,22 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
+void solve1() {
     int x, y, z;
     cin >> x >> y >> z;
 
     int answer = 0;
-    for(int n = x; n >= 0; n--) {
+    for (int n = x; n >= 0; n--) {
         int seat = n * y;
         int margin = (n + 1) * z;
 
-        if(seat + margin <= x) {
+        if (seat + margin <= x) {
             answer = n;
-			break;
+            break;
         }
     }
 
     cout << answer << endl;
-
-    return 0;
 }
+
+int main() { return 0; }
