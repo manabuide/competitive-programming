7822ca51daca63b1f96d87d3a2f524e357f7e3e7
manabuide <89676816+manabuide@users.noreply.github.com>
2021-10-16 01:52:40 +0900

    Add solve2 function

--- atcoder/abc/058/b.cpp
+++ atcoder/abc/058/b.cpp
@@ -14,4 +14,22 @@ void solve1() {
     cout << endl;
 }
 
+void solve2() {
+    string o, e;
+    cin >> o >> e;
+
+    string s;
+    size_t count_o = 0, count_e = 0;
+
+    for (size_t i = 0; i < e.size() + o.size(); ++i) {
+        if ((i + 1) % 2 == 0) {
+            s += e.at(count_e++);
+        } else {
+            s += o.at(count_o++);
+        }
+    }
+
+    cout << s << "\n"s;
+}
+
 int main() { return 0; }
