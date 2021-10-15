adb0c3f7f717f8a6a7f90bbb2becc94945fd26bf
manabuide <89676816+manabuide@users.noreply.github.com>
2021-10-16 01:04:27 +0900

    Move main function to solve1 function

--- atcoder/abc/092/b.cpp
+++ atcoder/abc/092/b.cpp
@@ -1,23 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-	int n, d, x;
-	cin >> n >> d >> x;
+void solve1() {
+    int n, d, x;
+    cin >> n >> d >> x;
 
-	int a[n];
-	for (int i = 0; i < n; i++) {
-		cin >> a[i];
-	}
+    int a[n];
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
 
-	int answer = x;
-	for (int i = 0; i < n; i++) {
-		int temp = (d - 1) / a[i] + 1;
+    int answer = x;
+    for (int i = 0; i < n; i++) {
+        int temp = (d - 1) / a[i] + 1;
 
-		answer += temp;
-	}
+        answer += temp;
+    }
 
-	cout << answer << endl;
-
-	return 0;
+    cout << answer << endl;
 }
+
+int main() { return 0; }
