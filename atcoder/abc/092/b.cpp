0c30d1e72f256fdcf1283e7949c23f53ae5107fe
manabuide <89676816+manabuide@users.noreply.github.com>
2021-10-16 01:12:05 +0900

    Add solve2 function

--- atcoder/abc/092/b.cpp
+++ atcoder/abc/092/b.cpp
@@ -20,4 +20,33 @@ void solve1() {
     cout << answer << endl;
 }
 
+void solve2() {
+    int n, d, x;
+    cin >> n >> d >> x;
+
+    vector<int> a;
+    for (int i = 0; i < n; ++i) {
+        int temp_a;
+        cin >> temp_a;
+        a.push_back(temp_a);
+    }
+
+    int total = 0;
+
+    for (int i = 0; i < n; ++i) {
+        int j = 0;
+        int day = 1;
+        while (true) {
+            day = j * a.at(i) + 1;
+            if (day > d) {
+                break;
+            }
+            ++j;
+        }
+        total += j;
+    }
+
+    cout << total + x << "\n"s;
+}
+
 int main() { return 0; }
