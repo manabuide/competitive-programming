0fea7f765903200de0515f5c2f260f9fb622f5a8
manabuide <89676816+manabuide@users.noreply.github.com>
2021-10-19 23:52:38 +0900

    Add a.cpp

---
+++ atcoder/abc/002/a.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    long long x, y;
+
+    cin >> x >> y;
+
+    if (x > y) {
+        cout << x << "\n"s;
+    } else {
+        cout << y << "\n"s;
+    }
+
+    return 0;
+}
